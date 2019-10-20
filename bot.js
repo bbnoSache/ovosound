ef7dbf590a04abf154b00a47f91a5ad4037a1d40
bbnoSache <54300676+bbnoSache@users.noreply.github.com>
2019-10-19 23:31:25 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,265 +1,120 @@
-const {Client, RichEmbed} = require('discord.js')
-const bot = new Client()
-let activated = '0'
+const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
+const prefix = "!"
+const client = new Discord.Client();
 
-bot.on('ready', ()=>{
-    console.log('Online.')
-    bot.user.setActivity(`yt.com/Sache`, {type: ("WATCHING")})
-    var Channel = bot.channels.get("613187138367913985");
-    Channel.fetchMessage("619053215618433071");
-})
+const queue = new Map();
 
-bot.on('raw', event =>{
-    const eventname = event.t
-    if(eventname === 'MESSAGE_REACTION_ADD')
-    {
-        var reactionChannel = bot.channels.get(event.d.channel_id);
-        if(event.d.message_id === '619053215618433071')
-        {
-            reactionChannel.fetchMessage(event.d.message_id)
-            .then(msg => {
-            var msgReaction = msg.reactions.get(event.d.emoji.name + ":" + event.d.emoji.id);
-            var user = bot.users.get(event.d.user_id)
-            })
-            .catch(err => console.log(err))
-        }
-        else {
-            reactionChannel.fetchMessage(event.d.message_id)
-            .then(msg => {
-            var msgReaction = msg.reactions.get(event.d.emoji.name + ":" + event.d.emoji.id);
-            var user = bot.users.get(event.d.user_id)
-            })
-            .catch(err => console.log(err))
-        }
-    }
+client.once('ready', () => {
+	console.log('Ready!');
 });
 
-bot.on('messageReactionAdd', (messageReaction, user) =>{
-    var roleName = messageReaction.emoji.name
-    var role = messageReaction.message.guild.roles.find("name", "Unverified");
-    var role2 = messageReaction.message.guild.roles.find("name", "N.W.A ★");
-    var role3 = messageReaction.message.guild.roles.find("name", "N.W.A ★");
-    console.log(roleName)
-    var member = messageReaction.message.guild.members.find(member => member.id === user.id);
-    if(member)
-    {
-        if(roleName === 'Sache'){
-            member.removeRole(role.id)
-            member.addRole(role2.id)
-            member.addRole(role3.id)
-            console.log("Success.")
-        }
-    }
-})
-
-bot.on('message', async message => {
-    let blacklisted = ['Hoes Mad']
-  
-    
-    let foundInText = false;
-    for (var i in blacklisted) {
-      if (message.content.toLowerCase().includes(blacklisted[i].toLowerCase())) foundInText = true;
-    }
-    
-  
-    
-      if (foundInText) {
-        message.channel.sendMessage('' + message.author + ' https://youtu.be/czd4GL4RZVs')
-    }
+client.once('reconnecting', () => {
+	console.log('Reconnecting!');
 });
 
-bot.on('guildMemberAdd', member=>{
-    let channel = member.guild.channels.find(channel => channel.id === "612755543940923392")
-    const embed = new RichEmbed()
-    .setAuthor(`Welcome to Sache's Traphouse`, `https://cdn.discordapp.com/attachments/612985420350750733/629140883664535563/ForDisc.png`, ``)
-    .setDescription(`Peep the ***Rules*** channel to see how to gain access to the server, ${member}`)
-    .setImage(`https://media.giphy.com/media/hU3QP6oTyxYdGtIIRI/giphy.gif`)
-    .setColor(0x000000);
-    channel.sendEmbed(embed)
-})
+client.once('disconnect', () => {
+	console.log('Disconnect!');
+});
 
-bot.on('guildMemberRemove', member=>{
-    let channel = member.guild.channels.find(channel => channel.id === "612755543940923392")
-    const embed = new RichEmbed()
-    .setAuthor(`Smh`, ``, ``)
-    .setDescription(` Damn, ${member} has left the Trap House, Chief said this wasn't it.`)
-    .setImage(``)
-    .setColor(0xffffff);
-    channel.sendEmbed(embed)
-})
+client.on('message', async message => {
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
 
-bot.on('message', async message => {
-    let blacklisted = ['nigger', 'faggot', '⛓']
-  
-    
-    let foundInText = false;
-    for (var i in blacklisted) {
-      if (message.content.toLowerCase().includes(blacklisted[i].toLowerCase())) foundInText = true;
-    }
-    
-  
-    
-      if (foundInText) {
-        message.delete();
-        message.channel.sendMessage('Chill out ' + message.author + ', before I yeet that ass straight out this server no cap.')
-    }
-  });
+	const serverQueue = queue.get(message.guild.id);
 
-bot.on('message', msg =>{
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
-    if(msg.author.id === '588977246744608778')
-    {
-        if(msg.content === '-activate')
-        {
-            activated = '1'
-        }
-        if(msg.content === '-deactivate')
-        {
-            activated = '0'
-        }
-    }
-    if(msg.author.id === '588977246744608778')
-    {
-        if(activated === '1')
-        {
-            console.log('wtf')
-            let content = msg.content
-            msg.channel.bulkDelete('1')
-            msg.channel.send(content)
-        }
-    }
-    let args = msg.content.split(' ')
-    switch(args[0]){
-        case '!dm':
-            if(msg.author.id === "588977246744608778"){
-                let mentioned = msg.mentions.users.first()
-                let message = args.join(' ').slice(25)
-                msg.channel.bulkDelete('1')
-                mentioned.send(message)   
-            }
-        break;
-        case '!kick':
-            if(!msg.member.hasPermission("KICK_MEMBERS")) return msg.channel.sendMessage("Boy you ain't got the juice to do that.");
-            if(!msg.guild.me.hasPermission("KICK_MEMBERS")) return msg.channel.sendMessage("Boy you ain't got the juice to do that.");
-            if(!args[1]) return msg.channel.sendMessage('Who we finna yeet?')
-            const tuser = msg.mentions.users.first();
-            const kreason = args.join(" ").slice(28);
-            if(tuser){
-                const member = msg.guild.member(tuser)
-                if(member){
-                    if(!kreason){
-                        member.kick('You were kicked.');
-                        const kembed = new RichEmbed()
-                        .setTitle('Get the fuck out.')
-                        .addField("User", tuser)
-                        msg.channel.sendEmbed(kembed);
-                    }
-                    else{
-                        member.kick(kreason);
-                        const kembed = new RichEmbed()
-                        .setTitle('Get the fuck out.')
-                        .addField("User", tuser)
-                        msg.channel.sendEmbed(kembed);
-                    }
-                }
-            }
-        break;
-        case '!ban':
-            if(!msg.member.hasPermission("BAN_MEMBERS")) return msg.channel.sendMessage("Boy you ain't got the juice to do that.");
-            if(!msg.guild.me.hasPermission("BAN_MEMBERS")) return msg.channel.sendMessage("Boy you ain't got the juice to do that.");
-            if(!args[1]) return msg.channel.sendMessage('Which homie are we eviscerating?')
-            const user = msg.mentions.users.first();
-            const breason = args.join(" ").slice(27);
-            if(user){
-                const member = msg.guild.member(user)
-                if(member){
-                    if(!breason){
-                        member.ban('You were banned.');
-                        const bembed = new RichEmbed()
-                        .setTitle('Get the fuck out.')
-                        .addField("User's name", user)
-                        .addField("User's ID", user.id)
-                        .addField("Reason", 'No reason specified.');
-                        msg.channel.sendEmbed(bembed);
-                    }
-                    else{
-                        member.ban(breason);
-                        const bembed = new RichEmbed()
-                        .setTitle('Get the fuck out.')
-                        .addField("User's name", user)
-                        .addField("User's ID", user.id)
-                        .addField("Reason", breason);
-                        msg.channel.sendEmbed(bembed);
-                    }
-                }
-            }
-        break;
-        case '!unban':
-            if(!msg.member.hasPermission("BAN_MEMBERS")) return msg.channel.sendMessage("You don't have the permission to unban someone!");
-            if(!msg.guild.me.hasPermission("BAN_MEMBERS")) return msg.channel.sendMessage("I don't have the allowed permission to unban someone!");
-            if(!args[1]) return msg.channel.sendMessage('Please specify a user ID!')
-            msg.guild.unban(args[1])
-            const uembed = new RichEmbed()
-            .setTitle('User has been unbanned!')
-            msg.channel.sendEmbed(uembed);
-        break;
-        case "!sotd":
-            let memberx = msg.mentions.users.first();
+	if (message.content.startsWith(`${prefix}play`)) {
+		execute(message, serverQueue);
+		return;
+	} else if (message.content.startsWith(`${prefix}skip`)) {
+		skip(message, serverQueue);
+		return;
+	} else if (message.content.startsWith(`${prefix}stop`)) {
+		stop(message, serverQueue);
+		return;
+	} else {
+		message.channel.send('You need to enter a valid command!')
+	}
+});
 
-            const embed = new RichEmbed()
-            .setAuthor(`SOTD - 9/9/2019 - Calm Snow - I See Stars `, `https://cdn.discordapp.com/attachments/601536698987577344/617144814902706196/Sache_New_Test.png`, ``)
-            .setTitle(`https://youtu.be/LXvkrEwy0Xs`)
-            .setColor(0x000000);
-             msg.channel.sendEmbed(embed);
-        break;
-        case "!latest":
-            const embed2 = new RichEmbed()
-            .setAuthor(`Peep Saches latest video, "No Video Available"`, `https://cdn.discordapp.com/attachments/601536698987577344/617144814902706196/Sache_New_Test.png`, ``)
-            .setTitle(`https://youtu.be/`)
-            .setColor(0x000000);
-             msg.channel.sendEmbed(embed2);
-        break;
-        case "Slap":
-            let memberz = msg.mentions.members.first();
-            let membery = msg.author;
+async function execute(message, serverQueue) {
+	const args = message.content.split(' ');
 
-            if(msg.mentions.users.size < 1) return msg.channel.send("You forgot to name a bitch to slap.");
-            msg.channel.send(`${membery} Just slapped the ***fuck*** out of ${memberz} :clap: `);
-        break;
-        case '!clear':
-            if(!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.channel.sendMessage("Boy stop trynna clear my chat.");
-            if(!msg.guild.me.hasPermission("MANAGE_MESSAGES")) return msg.channel.sendMessage("Boy stop trynna clear my chat.");
-            if(!args[1]) return msg.channel.sendMessage('How many messages we clearing chief?');
-            if(isNaN(parseFloat(args[1]))){
-                return msg.channel.sendMessage('Are you really trying to purge "' + args[1] + '" messages?');
-            }
-            msg.channel.bulkDelete(args[1]);
-        break;
-        case '!dmall':
-            if(msg.author.id === '588977246744608778'){
-            if(!args[1]) return msg.channel.sendMessage('?');
-            let dmGuild = msg.guild;
-            var message = msg.content.slice(6);
-            let memberarray = dmGuild.members.array();
-            let membercount = memberarray.length;
-            console.log(`Responding to ${msg.author.username} :  Sending message to all ${membercount} members of ${dmGuild.name}.`)
-            for (var i = 0; i < membercount; i++) {
-                if(!args[1]) return msg.channel.sendMessage('?');
-                let timeout = Math.floor((Math.random() * (10 - 0.01)) * 1000) + 10;
-                let member = memberarray[i];
-                sleep(timeout)
-                if(i == (membercount-1)) {
-                    console.log(`Waited ${timeout}ms.\t\\/\tDMing ${member.user.username}`);
-                } else {
-                    console.log(`Waited ${timeout}ms.\t|${i + 1}|\tDMing ${member.user.username}`);
-                }
-                member.send(`${message}`);
-            }
-            }
-        break;
-    }
-})
+	const voiceChannel = message.member.voiceChannel;
+	if (!voiceChannel) return message.channel.send('You need to be in a voice channel to play music!');
+	const permissions = voiceChannel.permissionsFor(message.client.user);
+	if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
+		return message.channel.send('I need the permissions to join and speak in your voice channel!');
+	}
+
+	const songInfo = await ytdl.getInfo(args[1]);
+	const song = {
+		title: songInfo.title,
+		url: songInfo.video_url,
+	};
+
+	if (!serverQueue) {
+		const queueContruct = {
+			textChannel: message.channel,
+			voiceChannel: voiceChannel,
+			connection: null,
+			songs: [],
+			volume: 5,
+			playing: true,
+		};
+
+		queue.set(message.guild.id, queueContruct);
+
+		queueContruct.songs.push(song);
+
+		try {
+			var connection = await voiceChannel.join();
+			queueContruct.connection = connection;
+			play(message.guild, queueContruct.songs[0]);
+		} catch (err) {
+			console.log(err);
+			queue.delete(message.guild.id);
+			return message.channel.send(err);
+		}
+	} else {
+		serverQueue.songs.push(song);
+		console.log(serverQueue.songs);
+		return message.channel.send(`${song.title} has been added to the queue!`);
+	}
+
+}
+
+function skip(message, serverQueue) {
+	if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to stop the music!');
+	if (!serverQueue) return message.channel.send('There is no song that I could skip!');
+	serverQueue.connection.dispatcher.end();
+}
+
+function stop(message, serverQueue) {
+	if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to stop the music!');
+	serverQueue.songs = [];
+	serverQueue.connection.dispatcher.end();
+}
+
+function play(guild, song) {
+	const serverQueue = queue.get(guild.id);
+
+	if (!song) {
+		serverQueue.voiceChannel.leave();
+		queue.delete(guild.id);
+		return;
+	}
+
+	const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+		.on('end', () => {
+			console.log('Music ended!');
+			serverQueue.songs.shift();
+			play(guild, serverQueue.songs[0]);
+		})
+		.on('error', error => {
+			console.error(error);
+		});
+	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+}
 
 bot.login(process.env.BOT_TOKEN)
